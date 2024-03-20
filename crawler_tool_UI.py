5b97566181a0d605ee5aa02a500dcc81500aeb58
Max <max284741991@gmail.com>
2024-03-20 22:30:36 +0800

    FEAT: 修改 crawler_tool_UI.py，新增 keyword 搜尋並回傳相關文章功能

--- crawler_tool_UI.py
+++ crawler_tool_UI.py
@@ -1,21 +1,24 @@
-import streamlit as st
 import time
+import streamlit as st
+from crawler import crawler_keyword_search
 
+# 設定頁面配置，設定頁面標題
 st.set_page_config(
     page_title= "新聞爬取工具"
 )
 
 st.title("新聞爬取工具")
-
 search_content = st.text_input(label = "輸入想搜尋的主題", placeholder="e.g. 台積電、特斯拉...")
-if st.button("搜尋"):
-    bar = st.progress(0)
-    for i in range(100):
-        bar.progress(i + 1, f"目前進度 {i + 1} %")
-        time.sleep(0.05)
-    bar.progress(100, "搜尋完成")
+if (st.button("搜尋") and search_content != "") or search_content != "":
+    # 根據搜尋的 keyword，返回相關的文章列表
+    url = "https://tw.news.yahoo.com/finance/"
+    link_list = crawler_keyword_search(url, search_content)
 
-    st.write("搜尋到的新聞網址")
-    url = "https://tw.news.yahoo.com/%E4%BF%84%E7%83%8F%E4%BA%A4%E6%8F%9B%E6%95%B8%E7%99%BE%E5%90%8D%E6%88%B0%E4%BF%98-%E9%96%8B%E6%88%B0%E4%BE%86%E7%AC%AC50%E6%AC%A1%E6%8F%9B%E5%9B%9A-134746621.html"
-
+    st.subheader("搜尋到的相關新聞", divider='rainbow')
+    # 印出各文章的相關資訊
+    for link_dict in link_list:
+        ID = link_dict["ID"]
+        title = link_dict["Title"]
+        link = link_dict["Link"]
+        st.write(f"{ID + 1}. [{title}]({link})")
 
