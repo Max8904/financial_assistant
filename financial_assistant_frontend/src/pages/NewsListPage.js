98c02c339b4d5097639145ec161b2302be9173e3
Max <max284741991@gmail.com>
2024-04-06 13:06:46 +0800

    FEAT: 新增 state 和 hook，並從 server 獲取資料

--- financial_assistant_frontend/src/pages/NewsListPage.js
+++ financial_assistant_frontend/src/pages/NewsListPage.js
@@ -1,12 +1,25 @@
-import React from 'react'
-import newsList from '../assets/data'
+import React, {useState, useEffect} from 'react'
+// import newsList from '../assets/data'
 import ListItem from '../components/ListItem'
 
 const NewsListPage = () => {
+
+  let [newsList, setNewsList] = useState([])
+
+  useEffect(() => {
+    getNews()
+  }, [])
+
+  let getNews = async () => {
+    let response = await fetch('http://localhost:8000/notes')
+    let data = await response.json()
+    setNewsList(data)
+  }
+
   return (
     <div className="news">
       <div className="news-header">
-        <h2 className="news-title">News</h2>
+        <h2 className="news-title">&#9782; News</h2>
         <p className="news-count">{newsList.length}</p>
       </div>
       <div className="news-list">
--- financial_assistant_frontend/src/pages/NewsPage.js
+++ financial_assistant_frontend/src/pages/NewsPage.js
@@ -1,15 +1,28 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {
   Link,
   useParams
 } from 'react-router-dom'
-import newsList from '../assets/data'
+// import newsList from '../assets/data'
 import {ReactComponent as ArrowLeft} from '../assets/arrow-left.svg'
 
 
 const NewsPage = (props) => {
   let newsId = useParams().id
-  let news = newsList.find(news => news.id === Number(newsId))
+
+  // let news = newsList.find(news => news.id === Number(newsId))
+  let [news, setNews] = useState(null)
+
+  useEffect(() => {
+    getNews()
+  }, [newsId])
+
+  let getNews = async () => {
+    let response = await fetch(`http://localhost:8000/notes/${noteId}`)
+    let data = await response.json()
+    setNews(data)
+  }
+  
   return (
     <div className="news">
       <div className="news-header">
